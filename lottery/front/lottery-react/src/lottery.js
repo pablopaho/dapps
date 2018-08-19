19a67940d133d3c120c91e9dcdd670e3a30aca8b
pablopaho <pablopaho@gmail.com>
2018-08-19 17:37:37 -0500

    first dapp with smart contract deployed in rinkeby

---
+++ lottery/front/lottery-react/src/lottery.js
@@ -0,0 +1,6 @@
+import web3 from './web3';
+
+const address = '0x1be8107f546b4d41B10B9871172d9E906b36cc31';
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
---
+++ lottery/smart-contract/lottery-v2/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'absurd exact tennis start camp cook begin primary boost arrange harvest equal',
+    'https://rinkeby.infura.io/xer3J47o9UYNETEup7In'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: '0x' + bytecode })
+          .send({ gas: '1000000', from: accounts[0] });
+
+    console.log(interface);
+    console.log('Contract deployed to', result.options.address );
+};
+deploy();
